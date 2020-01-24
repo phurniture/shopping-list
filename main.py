525399d88dab07352cf32f334d491d78d3981c1c
cooper <coopergrinnell@gmail.com>
2020-01-24 14:17:20 -0500

    got rid of the global variable by putting it in an extra loop, its yucky

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ Create a CLI application to store a shopping list.  Use cases:
     Use can exit the program runtime
 '''
 
-num = 1
 
 menuOption = None
 
@@ -23,43 +22,46 @@ menuText = '''
 5.) load list form file
 6.) exit
 '''
+while 1:
+    num = 1
 
-while menuOption != '6':
-    item = ''
-    print(menuText)
-    menuOption = input('Enter selection\n')
-    #print(menuOption)
-#1 add item
-    if menuOption == '1':
-        print('\nadd item')
-        item = input("name of item: ")
-        if item.strip() == '':
-            print('item cannot be empty')
-        elif item in mylist:
-            print('item already in list')
+    while menuOption != '6':
+        item = ''
+        print(menuText)
+        menuOption = input('Enter selection\n')
+        #print(menuOption)
+    #1 add item
+        if menuOption == '1':
+            print('\nadd item')
+            item = input("name of item: ")
+            if item.strip() == '':
+                print('item cannot be empty')
+            elif item in mylist:
+                print('item already in list')
+            else:
+                mylist.append(str(num) + '.) ' + item)
+                num += 1
+    #2 print list
+        elif menuOption == '2':
+            print('\nprint list')
+            print(mylist)
+    #3 remove item
+        elif menuOption == '3':
+            print('\nremove item by number')
+            item = input("number of item: ")
+            del mylist[(int(item)-1)]
+    #4 save to file
+        elif menuOption == '4':
+            print('\nsave list to file')
+            print("operation not implemented yet")
+    #5 load file  
+        elif menuOption == '5':
+            print('\nload list form file')
+            print("operation not implemented yet")
+    #6 exit
+        elif menuOption == '6':
+            print('exit')
+    #else
         else:
-            mylist.append(str(num) + '.) ' + item)
-            num += 1
-#2 print list
-    elif menuOption == '2':
-        print('\nprint list')
-        print(mylist)
-#3 remove item
-    elif menuOption == '3':
-        print('\nremove item by number')
-        item = input("number of item: ")
-        del mylist[(int(item)-1)]
-#4 save to file
-    elif menuOption == '4':
-        print('\nsave list to file')
-        print("operation not implemented yet")
-#5 load file  
-    elif menuOption == '5':
-        print('\nload list form file')
-        print("operation not implemented yet")
-#6 exit
-    elif menuOption == '6':
-        print('exit')
-#else
-    else:
-        print("not recogonized")
+            print("not recogonized")
+    break
